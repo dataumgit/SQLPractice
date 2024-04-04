d5c736169cc4e9df2ef0a1e152766cf76e38df09
Whitejun <masterdev@126.com>
2024-04-04 11:42:31 +0800

    load and export data operation

--- HiveSQLPractice.sql
+++ HiveSQLPractice.sql
@@ -118,3 +118,137 @@ drop table dept;
 
 -- 5） 管理表和外部表之间的互换
 
+desc formatted student2;
+alter table student2 set tblproperties('EXTERNAL' = 'TRUE');
+desc formatted students2;
+
+alter table student2 set tblproperties('EXTERNAL' = 'FALSE');
+desc formatted student2;
+
+
+-- ### 修改表
+ALTER TABLE table_name RENAME TO new_table_name;
+alter table student3 rename to student3rename;
+
+-- ## 增加/修改/替换列信息
+
+-- 增加
+ desc dept;
+alter table dept add columns(deptdesc string);
+
+-- 更新
+alter table dept change column deptdesc desc string;
+
+-- 替换列
+alter table dept replace columns(deptno string, dname string, loc string);
+
+---  #######################################
+
+-- # DML数据操作
+
+-- ## 1 数据导入
+
+-- 1.1 向表中装载数据（Load）
+-- 建表
+create table student(
+id string,
+name string
+)
+row format delimited fields terminated by '\t';
+
+--load local
+load data local inpath '/opt/module/hive/datas/mydb2' into table mydb2.student;
+
+-- load data on HDFS
+truncate table student;
+
+dfs -ls    /mydb2/;
+dfs -put    /opt/module/hive/datas/mydb2/student.txt      /mydb2/student;
+dfs -cat   /mydb2/student/student.txt;
+
+load data inpath '/mydb2/student/student_copy_1.txt' into table mydb2.student;
+
+load data inpath '/mydb2/student/student_copy_1.txt' overwrite into table mydb2.student;
+
+
+-- 1.2 通过查询语句向表中插入数据（Insert）
+--1) 建表
+   create table student_par(
+    id int,
+    name string
+    )
+row format delimited fields terminated by '\t';
+
+--2) 插入基本的数据
+    insert into table student_par values(1,'wangwu'),(2,'zhaoliu');
+    insert into table student_par select id,name from student;
+    insert overwrite table student_par select id,name from student;
+
+/*最直接的方式就是更换计算引擎
+
+-- SET hive.execution.engine=mr;
+	开启本地mr：
+    set hive.exec.mode.local.auto=true; 
+**/
+
+-- 1.3 查询语句中创建表并加载数据（As Select）
+create table if not exists student3
+as select id,name form student;
+-- .1.4 创建表时通过Location指定加载数据路径
+
+dfs -mkdir /student
+dfs -put    /opt/module/hive/datas/mydb2/student.txt /student;
+
+create external table if not exists student5(
+id int,
+name string
+)
+
+row format delimited fields terminated by '\t'
+location '/student';
+
+
+select  * from  student5;
+
+-- 1.5 Import数据到指定Hive表中
+
+ import table student2  from
+ '/user/hive/warehouse/export/student';
+
+-- ## 2 数据导出
+
+-- 2.1 Insert导出
+
+insert overwrite local directory '/opt/module/hive/datas/mydb2/export'
+            select * from student5;
+
+-- 结果格式化导出
+insert overwrite local directory '/opt/module/hive/datas/mydb2/export/student1'
+           ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' 
+select * from student;
+
+-- hdfs
+ insert overwrite directory '/user/atguigu/student2'
+             ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' 
+             select * from student;
+
+
+-- 2.2 Hadoop命令导出到本地
+desc formatted student;
+
+dfs -get /mydb2/student/student.txt /opt/module/hive/datas/mydb2/export/student3.txt;
+
+
+-- 2.3 Hive Shell 命令导出
+bin/hive -e 'select * from default.student;' > /opt/module/hive/datas/export/student4.txt;
+
+
+-- .2.4 Export导出到HDFS上
+
+export table default.student to '/user/hive/warehouse/export/student';
+
+
+-- 2.5 Sqoop导出
+
+-- 2.6 清除表中数据（Truncate）
+truncate table student;
